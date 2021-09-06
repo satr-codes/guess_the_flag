5ed000f1f056b9924a858f69eedbd2f28b2d463e
Khalid Alhazmi <alhazmi@outlook.com>
2021-09-06 14:00:21 +0300

    add starter code

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({Key? key}) : super(key: key);
+  List<String> countries = [
+    "Estonia",
+    "France",
+    "Germany",
+    "Ireland",
+    "Italy",
+    "Monaco",
+    "Nigeria",
+    "Poland",
+    "Russia",
+    "Spain",
+    "UK",
+    "US"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
